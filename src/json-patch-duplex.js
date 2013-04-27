5d6419efe4039f481fe665d9c02a7b136b247aa5
John Rogelstad <john@xtuple.com>
2013-04-27 15:05:17 -0300

    Update json-patch-duplex.js
    Modify generate function to ensure dates are the same type.

--- src/json-patch-duplex.js
+++ src/json-patch-duplex.js
@@ -222,6 +222,7 @@ var jsonpatch;
     jsonpatch.generate = generate;
     // Dirty check if obj is different from mirror, generate patches and update mirror
     function _generate(mirror, obj, patches, path) {
+        obj = JSON.parse(JSON.stringify(obj));
         var newKeys = Object.keys(obj);
         var oldKeys = Object.keys(mirror);
         var changed = false;
