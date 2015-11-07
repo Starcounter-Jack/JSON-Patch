fb6c6461c711b1b245cf3bf982db7c7a8ce48fb2
miyconst <const.miyang@gmail.com>
2015-11-07 23:57:26 +0300

    Improve  _objectKeys method
    https://github.com/Starcounter-Jack/JSON-Patch/issues/85

--- src/json-patch-duplex.ts
+++ src/json-patch-duplex.ts
@@ -27,7 +27,13 @@ module jsonpatch {
 
   var _objectKeys = function (obj) {
     if (_isArray(obj)) {
-      return Array.apply(null, { length: obj.length }).map(function (e, i, a) { return i.toString(); });
+      var keys = new Array(obj.length);
+
+      for (var i = 0; i < keys.length; i++) {
+        keys[i] = i.toString();
+      }
+
+      return keys;
     }
 
     if (Object.keys) {
