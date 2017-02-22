01dd6e7465ceff12451b08b0a23f17e33491cb5c
Tomek Wytrebowicz <tomalecpub@gmail.com>
2017-02-22 18:19:56 +0100

    Use `setup` for benchmarks,
    to test only applicable function calls

--- test/spec/duplexBenchmark.js
+++ test/spec/duplexBenchmark.js
@@ -1,4 +1,3 @@
-var obj, obj2, patches;
 if (typeof window === 'undefined') {
     var jsdom = require("jsdom").jsdom;
     var doc = jsdom(undefined, undefined);
@@ -18,58 +17,89 @@ if (typeof Benchmark === 'undefined') {
 
 
 var suite = new Benchmark.Suite;
-suite.add('generate operation', function() {
-    obj = {
-        firstName: "Albert",
-        lastName: "Einstein",
-        phoneNumbers: [{
-            number: "12345"
-        }, {
-            number: "45353"
-        }]
-    };
-    var observer = jsonpatch.observe(obj);
+suite.add('generate operation', {
+    setup: function(){
+        var obj = {
+            firstName: "Albert",
+            lastName: "Einstein",
+            phoneNumbers: [{
+                number: "12345"
+            }, {
+                number: "45353"
+            }]
+        };
+        var observer = jsonpatch.observe(obj);
+    },
+    fn: function() {
 
-    obj.firstName = "Joachim";
-    obj.lastName = "Wester";
-    obj.phoneNumbers[0].number = "123";
-    obj.phoneNumbers[1].number = "456";
+        obj.firstName = "Joachim";
+        obj.lastName = "Wester";
+        obj.phoneNumbers[0].number = "123";
+        obj.phoneNumbers[1].number = "456";
 
-    var patches = jsonpatch.generate(observer);
-    obj2 = {
-        firstName: "Albert",
-        lastName: "Einstein",
-        phoneNumbers: [{
-            number: "12345"
-        }, {
-            number: "45353"
-        }]
-    };
-
-    jsonpatch.apply(obj2, patches);
+        var patches = jsonpatch.generate(observer);
+    }
 });
-suite.add('compare operation', function() {
-    var obj = {
-        firstName: "Albert",
-        lastName: "Einstein",
-        phoneNumbers: [{
-            number: "12345"
-        }, {
-            number: "45353"
-        }]
-    };
-    var obj2 = {
-        firstName: "Joachim",
-        lastName: "Wester",
-        mobileNumbers: [{
-            number: "12345"
-        }, {
-            number: "45353"
-        }]
-    };
+suite.add('generate operation and re-apply', {
+    setup: function(){
+        var obj = {
+            firstName: "Albert",
+            lastName: "Einstein",
+            phoneNumbers: [{
+                number: "12345"
+            }, {
+                number: "45353"
+            }]
+        };
+        var observer = jsonpatch.observe(obj);
+    },
+    fn: function() {
 
-    var patches = jsonpatch.compare(obj, obj2);
+        obj.firstName = "Joachim";
+        obj.lastName = "Wester";
+        obj.phoneNumbers[0].number = "123";
+        obj.phoneNumbers[1].number = "456";
+
+        var patches = jsonpatch.generate(observer);
+        obj2 = {
+            firstName: "Albert",
+            lastName: "Einstein",
+            phoneNumbers: [{
+                number: "12345"
+            }, {
+                number: "45353"
+            }]
+        };
+
+        jsonpatch.apply(obj2, patches);
+    }
 });
+suite.add('compare operation', {
+    setup: function(){
+        var obj = {
+            firstName: "Albert",
+            lastName: "Einstein",
+            phoneNumbers: [{
+                number: "12345"
+            }, {
+                number: "45353"
+            }]
+        };
+        var obj2 = {
+            firstName: "Joachim",
+            lastName: "Wester",
+            mobileNumbers: [{
+                number: "12345"
+            }, {
+                number: "45353"
+            }]
+        };
+    },
+    fn: function() {
+        var patches = jsonpatch.compare(obj, obj2);
+    }
+});
+
 
 // if we are in the browser with benchmark < 2.1.2
 if(typeof benchmarkReporter !== 'undefined'){
