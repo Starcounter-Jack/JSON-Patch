4a788782e209723ff8d1b31f978eb824eb9ecf57
Tomek Wytrebowicz <tomalecpub@gmail.com>
2017-02-23 17:36:47 +0100

    1.1.8

--- src/json-patch-duplex.js
+++ src/json-patch-duplex.js
@@ -1,6 +1,6 @@
 /*!
  * https://github.com/Starcounter-Jack/JSON-Patch
- * json-patch-duplex.js version: 1.1.7
+ * json-patch-duplex.js version: 1.1.8
  * (c) 2013 Joachim Wester
  * MIT license
  */
