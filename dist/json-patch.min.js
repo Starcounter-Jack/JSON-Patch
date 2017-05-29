d765565f9802a8ee8a5f49bd880ea21c4a88a047
Omar Alshaker <omar@omaralshaker.com>
2017-05-29 12:48:46 +0200

    1.2.0

--- dist/json-patch.min.js
+++ dist/json-patch.min.js
@@ -1,6 +1,6 @@
 /*!
  * https://github.com/Starcounter-Jack/JSON-Patch
- * json-patch-duplex.js version: 1.1.10
+ * json-patch-duplex.js version: 1.2.0
  * (c) 2013 Joachim Wester
  * MIT license
  */
