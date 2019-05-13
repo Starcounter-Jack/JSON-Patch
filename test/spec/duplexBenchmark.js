b210dd6a1189d5ab2c31219cad83c242804a889f
Tom Pytleski <pytlesk4@gmail.com>
2019-05-13 11:35:21 -0500

    add benchmarks for generating test operations

--- test/spec/duplexBenchmark.js
+++ test/spec/duplexBenchmark.js
@@ -145,6 +145,136 @@ suite.add('compare operation same but deep objects', {
   }
 });
 
+// Benchmark generating test operations
+suite.add('generate operation, with inversible set to true', {
+  setup: function() {
+    var obj = {
+      firstName: 'Albert',
+      lastName: 'Einstein',
+      phoneNumbers: [
+        {
+          number: '12345'
+        },
+        {
+          number: '45353'
+        }
+      ]
+    };
+    var observer = jsonpatch.observe(obj, undefined, true);
+  },
+  fn: function() {
+    obj.firstName = 'Joachim';
+    obj.lastName = 'Wester';
+    obj.phoneNumbers[0].number = '123';
+    obj.phoneNumbers[1].number = '456';
+
+    var patches = jsonpatch.generate(observer);
+  }
+});
+suite.add('generate operation and re-apply, with inversible set to true', {
+  setup: function() {
+    var obj = {
+      firstName: 'Albert',
+      lastName: 'Einstein',
+      phoneNumbers: [
+        {
+          number: '12345'
+        },
+        {
+          number: '45353'
+        }
+      ]
+    };
+    var observer = jsonpatch.observe(obj, undefined, true);
+  },
+  fn: function() {
+    obj.firstName = 'Joachim';
+    obj.lastName = 'Wester';
+    obj.phoneNumbers[0].number = '123';
+    obj.phoneNumbers[1].number = '456';
+
+    var patches = jsonpatch.generate(observer);
+    obj2 = {
+      firstName: 'Albert',
+      lastName: 'Einstein',
+      phoneNumbers: [
+        {
+          number: '12345'
+        },
+        {
+          number: '45353'
+        }
+      ]
+    };
+
+    jsonpatch.applyPatch(obj2, patches);
+  }
+});
+suite.add('compare operation, with inversible set to true', {
+  setup: function() {
+    var obj = {
+      firstName: 'Albert',
+      lastName: 'Einstein',
+      phoneNumbers: [
+        {
+          number: '12345'
+        },
+        {
+          number: '45353'
+        }
+      ]
+    };
+    var obj2 = {
+      firstName: 'Joachim',
+      lastName: 'Wester',
+      mobileNumbers: [
+        {
+          number: '12345'
+        },
+        {
+          number: '45353'
+        }
+      ]
+    };
+  },
+  fn: function() {
+    var patches = jsonpatch.compare(obj, obj2, { inversible: true });
+  }
+});
+
+suite.add('compare operation same but deep objects, with inversible set to true', {
+  setup: function() {
+    var depth = 10;
+
+    function shallowObj() {
+      return {
+        shallow: {
+          firstName: 'Tomek',
+          lastName: 'Wytrebowicz',
+          mobileNumbers: [
+            {
+              number: '12345'
+            },
+            {
+              number: '45353'
+            }
+          ]
+        }
+      };
+    }
+    var obj = shallowObj();
+    var node = obj;
+    while (depth-- > 0) {
+      node.nested = shallowObj();
+      node = node.nested;
+    }
+    var obj2 = obj;
+  },
+  fn: function() {
+    var patches = jsonpatch.compare(obj, obj2, { inversible: true });
+  }
+});
+
 // if we are in the browser with benchmark < 2.1.2
 if (typeof benchmarkReporter !== 'undefined') {
   benchmarkReporter(suite);
