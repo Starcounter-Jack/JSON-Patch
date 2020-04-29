9347891694c4c28bd7dce73c222aaaa25b568ff8
Jose Luis Arenas <arenas@softblocks.io>
2020-04-29 14:38:37 +0100

    Fixes Starcounter-Jack/JSON-Patch#250

--- src/duplex.ts
+++ src/duplex.ts
@@ -163,7 +163,7 @@ function _generate(mirror, obj, patches, path, invertible) {
     if (hasOwnProperty(obj, key) && !(obj[key] === undefined && oldVal !== undefined && Array.isArray(obj) === false)) {
       var newVal = obj[key];
 
-      if (typeof oldVal == "object" && oldVal != null && typeof newVal == "object" && newVal != null) {
+      if (typeof oldVal == "object" && oldVal != null && typeof newVal == "object" && newVal != null && (Array.isArray(oldVal) === Array.isArray(newVal))) {
         _generate(oldVal, newVal, patches, path + "/" + escapePathComponent(key), invertible);
       }
       else {
