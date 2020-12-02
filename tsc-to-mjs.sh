7c4aba76943e37603ca954f19f1e9304ebfe674b
Oren Griffin <oren@optibus.com>
2020-12-02 09:16:24 +0200

    fix sed to work in mac

--- tsc-to-mjs.sh
+++ tsc-to-mjs.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 cd module
 mv core.js core.mjs && mv duplex.js duplex.mjs && mv helpers.js helpers.mjs
-sed -i 's/\.js/\.mjs/g' duplex.mjs core.mjs
+# Unlike Ubuntu, OS X requires the extension to be explicitly specified
+# https://myshittycode.com/2014/07/24/os-x-sed-extra-characters-at-the-end-of-l-command-error/
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' 's/\.js/\.mjs/g' duplex.mjs core.mjs
+else
+  sed -i 's/\.js/\.mjs/g' duplex.mjs core.mjs
+fi
