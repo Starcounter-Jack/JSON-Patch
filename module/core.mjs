5bcc925fc57374081ec2785d768c9fc2cf4dc71d
Alejandro Romero Herrera <alromh87@gmail.com>
2021-06-01 22:46:21 -0500

    Added compiled files

--- module/core.mjs
+++ module/core.mjs
@@ -183,8 +183,10 @@ export function applyOperation(document, operation, validateOperation, mutateDoc
         }
         while (true) {
             key = keys[t];
-            if (banPrototypeModifications && key == '__proto__') {
-                throw new TypeError('JSON-Patch: modifying `__proto__` prop is banned for security reasons, if this was on purpose, please set `banPrototypeModifications` flag false and pass it to this function. More info in fast-json-patch README');
+            if (banPrototypeModifications &&
+                (key == '__proto__' ||
+                    (key == 'prototype' && t > 0 && keys[t - 1] == 'constructor'))) {
+                throw new TypeError('JSON-Patch: modifying `__proto__` or `constructor/prototype` prop is banned for security reasons, if this was on purpose, please set `banPrototypeModifications` flag false and pass it to this function. More info in fast-json-patch README');
             }
             if (validateOperation) {
                 if (existingPathFragment === undefined) {
