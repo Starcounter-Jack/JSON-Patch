893db9847f79cd1baf3d11309af86cacb5048602
Michael Jarecki <4808737+superguineapig@users.noreply.github.com>
2021-09-29 14:05:25 -0500

    Add new helpers for extended ops

--- src/helpers.ts
+++ src/helpers.ts
@@ -172,3 +172,89 @@ export class PatchError extends Error {
 
 // exported for use in jasmine test
 export const PROTO_ERROR_MSG = 'JSON-Patch: modifying `__proto__` or `constructor/prototype` prop is banned for security reasons, if this was on purpose, please set `banPrototypeModifications` flag false and pass it to this function. More info in fast-json-patch README';
+
+export function isValidExtendedOpId(xid: string): boolean {
+    return typeof xid === 'string' && xid.length >= 3 && xid.indexOf('x-') === 0;
+}
+
+type KeyType = string | number;
+
+interface PruneComponents {
+    modType: 'prune';
+    comps: Array<KeyType>;
+};
+
+interface GraftComponents {
+    modType: 'graft';
+    comps: Array<KeyType>;
+};
+
+export type PathComponents = PruneComponents | GraftComponents;
+
+/**
+ * attach/remove source tree to/from target tree at appropriate path.
+ * modifies targetObj (in place) by reference.
+ * 
+ * This is necessary to deal with JS "by value" semantics
+ */
+export function _graftTree(sourceObj: any, targetObj: any, pathComponents: PathComponents): void {
+    if (pathComponents.comps.length === 0) {
+        // no changes
+        return;
+    }
+
+    // traverse document trees until at the appropriate parent level
+    let graftTgt = targetObj;
+    let graft = sourceObj;
+    let graftKey: KeyType = '';
+
+    // single component is top-level key
+    if (pathComponents.comps.length === 1) {
+        graftKey = pathComponents.comps[0];
+        if (pathComponents.modType === 'graft') {
+            graftTgt[graftKey] = graft[graftKey];
+        } else {
+            // top-level prune is a "best guess" that the provided key was removed from
+            // the top-level object (we have no visibility into what the extended
+            // operator has actually done since it comes from "user-space";
+            // external to the extension api)
+            // NOTE: pruning is here only to allow the extension api to
+            // emulate the RFC api; user-defined operations may perform complex
+            // removals, but they may not be visible during the prune process.
+            // It is recommended that the user stick to the RFC 'remove' operation
+            // and not implemennt their own removal operations in extended-space;
+            // or at least limit removal to the provided path, and not perform other mutations
+            // combined with removals
+            if (Array.isArray(targetObj)) {
+                graftTgt.splice(~~graftKey, 1);
+            } else {
+                delete graftTgt[graftKey];
+            }
+        }
+        return;
+    }
+
+    for (let i = 0; i < pathComponents.comps.length; i++) {
+        graftKey = pathComponents.comps[i];
+        graft = graft[graftKey];
+        // if there is no value in the target obj at the current key,
+        // than this is a graft point
+        if (pathComponents.modType === 'graft' && graftTgt[graftKey] === undefined) {
+            // if both target and source are undefined - No Op
+            if (graft === undefined) {
+                return;
+            }
+            break;
+        }
+        // there was a removal; the graft point needs to be the 2nd to last path comp
+        // a.k.a. the parent obj of the pruned key
+        // in order to preserve additional structure that was not pruned
+        if (i === pathComponents.comps.length - 2) {
+            break;
+        }
+        graftTgt = graftTgt[graftKey];
+    }
+
+    // graft
+    graftTgt[graftKey] = graft;
+}
